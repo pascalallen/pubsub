5b6928e2baf84d35363e4e6b915067207d915b7a
Pascal <pascal.allen88@gmail.com>
2023-03-23 12:31:52 -0500

    saving progress

--- subscriber.go
+++ subscriber.go
@@ -1,11 +1,37 @@
 package pubsub
 
-import "sync"
+import (
+	"sync"
+)
 
 type Subscriber struct {
 	Id       string
 	Messages chan *Message
-	Topics   map[string]bool
+	Topics   []string
 	Active   bool
 	Mutex    *sync.RWMutex
+}
+
+func (s *Subscriber) AddTopic(topic string) {
+	for _, t := range s.Topics {
+		if t == topic {
+			return
+		}
+	}
+
+	s.Mutex.RLock()
+	defer s.Mutex.RUnlock()
+	s.Topics = append(s.Topics, topic)
+}
+
+func (s *Subscriber) RemoveTopic(topic string) {
+	for i, t := range s.Topics {
+		if t == topic {
+			s.Topics[i] = s.Topics[len(s.Topics)-1]
+		}
+	}
+
+	s.Mutex.RLock()
+	defer s.Mutex.RUnlock()
+	s.Topics = s.Topics[:len(s.Topics)-1]
 }
